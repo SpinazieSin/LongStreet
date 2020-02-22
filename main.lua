1449f5b7dcfbc18b1e13bfc3751d79dd75593518
SpinazieSin <drone.grt@gmail.com>
2020-02-22 17:43:16 +0100

    Movement speed fix, sprite rendering, basic lighting

--- main.lua
+++ main.lua
@@ -31,29 +31,38 @@ function love.load()
     posx, posy = 22, 12
     dirx, diry = -1, 0
     planex, planey = 0, 0.66
-    movespeed, rotspeed = 0.01, 0.01
+    movespeed, rotspeed = 2, 2
     
+    -- turn the lights on
+    light = false
+
     -- h = screen height
     -- w = screen width
     h = 512
-    w = 600
+    w = 564
 
     -- unused
     texwidth = 512
-    texheight = 512
+    texheight = 450
 
     -- set gamescreen
-    fullscreen = true
+    fullscreen = falsea
     success = love.window.setMode( w, h, {fullscreen=fullscreen} )
 
     -- load canvas and images
     canvas = love.graphics.newCanvas(w, h)
     canvas = canvas:newImageData()
     imagedata = love.image.newImageData("brick.jpg")
+    spritedata = love.image.newImageData("example.png")
+    spriteim = love.graphics.newImage(spritedata)
+    sprite = {x = 20.5, y = 11.5}
 end
  
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
+
+ local movespeed = movespeed * dt
+ local rotspeed = rotspeed * dt
  if love.keyboard.isDown("w") then
   if map[math.floor(posx + dirx * movespeed)][math.floor(posy)] < 1 then
    posx = posx + dirx * movespeed
@@ -96,30 +105,81 @@ function love.draw()
   local cnvs = draw3d()
   local screentodraw = love.graphics.newImage(cnvs)
   love.graphics.draw(screentodraw)
+  cnvs = drawsprites(cnvs)
   collectgarbage('collect')
+end
+
+function drawsprites(cnvs)
+  local spritex = sprite.x - posx
+  local spritey = sprite.y - posy
+  local dist = math.sqrt((spritex^2) * (spritey^2))
+  local localspritedata = spritedata:clone()
+
+    --transform sprite with the inverse camera matrix
+    -- [ planeX   dirX ] -1                                       [ dirY      -dirX ]
+    -- [               ]       =  1/(planeX*dirY-dirX*planeY) *   [                 ]
+    -- [ planeY   dirY ]                                          [ -planeY  planeX ]
+
+  local invdet = 1.0 / (planex * diry - dirx * planey) --required for correct matrix multiplication
+
+  local transformx = invdet * (diry * spritex - dirx * spritey)
+  local transformy = invdet * (-planey * spritex + planex * spritey) --this is actually the depth inside the screen, that what Z is in 3D
+
+  -- sprite is behind player
+  if transformy < 0 then
+    return
+  end
+
+  local spritescreenx = math.floor((w / 2) * (1 + transformx / transformy))
+  if spritescreenx < 0-localspritedata:getWidth() or spritescreenx > w+localspritedata:getWidth() then
+    return
+  end
+
+  --calculate height of the sprite on screen
+  local spriteheight = math.abs(math.floor(h / (transformy))) --using 'transformY' instead of the real distance prevents fisheye
+  
+  --calculate lowest and highest pixel to fill in current stripe
+  local drawstarty = -spriteheight / 2 + h / 2
+  local drawendy = spriteheight / 2 + h / 2
+
+  --calculate width of the sprite
+  local spritewidth = math.abs( math.floor(h / (transformy)))
+  local drawstartx = -spritewidth / 2 + spritescreenx
+  if (drawstartx < 0) then
+    drawstartx = 0
+  end
+  local drawendx = spritewidth / 2 + spritescreenx
+  if (drawendx >= w) then
+    drawendx = w - 1
+  end
+
+  localspritedata:mapPixel(function(x, y, r, g, b, a) return r, g, b, a/(dist*0.8) end)
+  spriteim:replacePixels(localspritedata)
+  love.graphics.draw(spriteim, spritescreenx, drawstarty, 0, 1/dist)
+  return cnvs
 end
 
 function draw3d()
  local cnvs = canvas:clone()
 
  for x=0,w do
-  camerax = (2 * x) / w - 1
+  local camerax = (2 * x) / w - 1
   
-  raydirx = dirx + planex * camerax
-  raydiry = diry + planey * camerax
+  local raydirx = dirx + planex * camerax
+  local raydiry = diry + planey * camerax
 
-  mapx = math.floor(posx)
-  mapy = math.floor(posy)
+  local mapx = math.floor(posx)
+  local mapy = math.floor(posy)
 
-  deltadistx = math.abs(1 / raydirx)
-  deltadisty = math.abs(1 / raydiry)
+  local deltadistx = math.abs(1 / raydirx)
+  local deltadisty = math.abs(1 / raydiry)
 
   -- step direction
-  stepx = 0
-  stepy = 0
+  local stepx = 0
+  local stepy = 0
 
-  wallhit = false
-  side = 0
+  local wallhit = false
+  local side = 0
 
   if raydirx < 0 then
    stepx = -1
@@ -136,8 +196,8 @@ function draw3d()
    sidedisty = (mapy + 1 - posy) * deltadisty
   end
 
-  wallcolor = 8
-  count = 0
+  local wallcolor = 0
+  local count = 0
   while not(wallhit) do
    if sidedistx < sidedisty then
     sidedistx = sidedistx + deltadistx
@@ -148,23 +208,27 @@ function draw3d()
     mapy = mapy + stepy
     side = 1
    end
-   if map[mapx][mapy] > 0 or count > 50 then
+   if map[mapx][mapy] > 0 then
     wallhit = true
     wallcolor = map[mapx][mapy]
+    break
+   elseif count > 15 then
+    wallhit = false
     break
    end
    count = count + 1
   end
   
   if wallhit then
+   local perpwalldist = 0
    if side == 0 then
     perpwalldist = (mapx - posx + (1 - stepx) / 2) / raydirx
    else
     perpwalldist = (mapy - posy + (1 - stepy) / 2) / raydiry
    end
 
-   lineheight = (h / perpwalldist)
-   drawstart = -lineheight / 2 + h / 2
+   local lineheight = (h / perpwalldist)
+   local drawstart = -lineheight / 2 + h / 2
    if drawstart < 0 then
     drawstart = 0
    end
@@ -177,7 +241,7 @@ function draw3d()
     -- texNum = worldMap[mapX][mapY] - 1 --1 subtracted from it so that texture 0 can be used!
 
     --calculate value of wallX
-    wallx = 0 --where exactly the wall was hit
+    local wallx = 0 --where exactly the wall was hit
     if (side == 0) then
       wallx = posy + perpwalldist * raydiry
     else
@@ -186,44 +250,38 @@ function draw3d()
     wallx = wallx - math.floor(wallx)
 
     --x coordinate on the texture
-    texx = wallx * texwidth
+    local texx = wallx * texwidth
     if (side == 0 and raydirx > 0) then
       texx = texwidth - texx - 1
     end
     if (side == 1 and raydiry < 0) then
       texx = texwidth - texx - 1
     end
-    texx = texx + 0.5
 
     -- How much to increase the texture coordinate per screen pixel
-    step = 1.0 * texheight / lineheight
+    local step = 1.0 * texheight / lineheight
     -- Starting texture coordinate
-    texpos = (drawstart - h / 2 + lineheight / 2) * step + 1
+    local texpos = (drawstart - h / 2 + lineheight / 2) * step + 1
     local imageheight = imagedata:getHeight()
     local imagewidth = imagedata:getWidth()
+    local fade = perpwalldist*perpwalldist/4
     if texx > 0 and x < w then
       for y=drawstart,drawend do
         -- Cast the texture coordinate to integer, and mask with (texHeight - 1) in case of overflow
-        texy = texpos
+        local texy = texpos
         if texx > imagewidth or texy > imageheight then
           break
         end
         texpos = texpos + step
 
         local r, g, b, a = imagedata:getPixel(texx, texy)
-        cnvs:setPixel(x, y, r, g, b, a)
-        --make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
-        --if (side == 1) color = (color >> 1) & 8355711
+        cnvs:setPixel(x, y, r, g, b, a/fade)
       end
     end
-    -- x = imagedata:gg()
-
     -- DRAWBUFFER
-    if not(fullscreen) then
-     -- love.graphics.line(x, 0, x, drawstart)
-     -- love.graphics.line(x, drawstart, x, drawend)
-     -- love.graphics.line(x, drawend, x, h)
-    end
+    -- love.graphics.line(x, 0, x, drawstart)
+    -- love.graphics.line(x, drawstart, x, drawend)
+    -- love.graphics.line(x, drawend, x, h)
   end
  end
  return cnvs
